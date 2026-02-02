a4f152003c1359927a79d1e2c8e74d677d320d2a
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-02-02 12:50:15 +0000

    worked on sql tasks

--- session_1/2_practice/code.sql
+++ session_1/2_practice/code.sql
@@ -9,3 +9,34 @@
 
 
 -- write your sql code here
+
+SELECT Books.title, Members.name, Loans.loan_date
+FROM 
+Books JOIN Loans
+ON Books.id = Loans.book_id
+JOIN Members
+ON Loans.member_id = Members.id;
+
+
+SELECT Books.title, Loans.loan_date
+FROM 
+Loans JOIN Books
+ON Loans.book_id = Books.id;
+
+SELECT LibraryBranch.name AS Library, Books.title
+FROM
+Books JOIN LibraryBranch
+ON Books.branch_id = LibraryBranch.id;
+
+SELECT LibraryBranch.name AS Library, COUNT(Books.title)
+FROM 
+Books JOIN LibraryBranch
+ON Books.branch_id = LibraryBranch.id
+GROUP BY Library;
+
+SELECT LibraryBranch.name AS Library, COUNT(Books.title) AS Amount
+FROM 
+Books JOIN LibraryBranch
+ON Books.branch_id = LibraryBranch.id
+WHERE Amount > 7
+GROUP BY Library; 
