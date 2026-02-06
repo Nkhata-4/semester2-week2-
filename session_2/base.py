d8320a53ba3474c6a03ccc2b873576288b0fdac4
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-02-06 10:48:22 +0000

    worked up to task 6

--- session_2/base.py
+++ session_2/base.py
@@ -3,18 +3,37 @@ import sqlite3
 import pandas as pd
 import matplotlib as mpl
 
+
 def get_connection(db_path="orders.db"):
     """
     Establish a connection to the SQLite database.
     Returns a connection object.
     """
-    conn = sqlite3.connect(db_path)
+    conn = sqlite3.connect("orders.db")
+
     conn.row_factory = sqlite3.Row
+
     return conn
 
 def main():
 
     db = get_connection()
+
+    query = """SELECT orders.customer_id,
+    order_items.quantity * products.price AS TotalSpend
+    FROM 
+    orders JOIN order_items
+    ON orders.order_id = order_items.order_id
+    JOIN products
+    ON order_items.product_id = products.product_id 
+    GROUP BY orders.customer_id
+    ORDER BY TotalSpend DESC LIMIT 5;"""
+
+
+    cursor = db.cursor()
+
+    for row in cursor.execute(query):
+        print(list(row))
 
     db.close()
 
